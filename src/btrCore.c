c71c1ce8bbd46a5b39a273e43bcb6876c08fdc33
natrajmuthusamy <52849136+natrajmuthusamy@users.noreply.github.com>
2025-11-21 21:21:40 +0530

    DELIA-69579 : BT speaker AutoReconnect failed on Llama.
    Reason for change:
    Marked the device as connected during the connection process, except for HID devices.
    
    Priority: P1
    Test Procedure: Follow the steps provided in descriptio
    
    Risks: High
    Signed-off-by:Natraj Muthusamy <Natraj_Muthusamy@comcast.com>

--- src/btrCore.c
+++ src/btrCore.c
@@ -4755,8 +4755,10 @@ BTRCore_ConnectDevice (
     BTRCORELOG_INFO ("Connected to device %s Successfully. = %llu\n", lpcBTRCoreBTDeviceName, aBTRCoreDevId);
     /* Should think on moving a connected LE device from scanned list to paired list */
 
+    if (lenBTDeviceType != enBTDevHID) {
+        lpstBTRCoreBTDevice->bDeviceConnected = TRUE;
+    }
 
-	
     if ((lpstBTRCoreDevStateInfo->eDevicePrevState != enBTRCoreDevStConnected) &&
         (lpstBTRCoreDevStateInfo->eDeviceCurrState != enBTRCoreDevStPlaying)) {
         lpstBTRCoreDevStateInfo->eDevicePrevState  = lpstBTRCoreDevStateInfo->eDeviceCurrState;
